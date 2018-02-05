86df5c1ab4aaf839f76294d0ada870f10956f562
Bala Krishna Ragala <baluragala@gmail.com>
2018-02-05 17:18:05 +0530

    test cases added

---
+++ src/Link.test.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Enzyme, { shallow, render, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { shallowToJson } from "enzyme-to-json";
+import Link from "./Link";
+
+// React 16 Enzyme adapter
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("Link", () => {
+  it("should render correctly", () => {
+    //snapshot testing
+    const wrapper = shallow(<Link title="mockTitle" url="mockUrl" />);
+    //debug
+    // console.log(wrapper.debug());
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+
+  it("should handle the click event", () => {
+    //simulating event
+    window.alert = jest.fn();
+    const wrapper = shallow(<Link title="mockTitle" url="mockUrl" />);
+    wrapper.simulate("click");
+    expect(window.alert).toHaveBeenCalledWith("clicked!");
+  });
+
+  it("should handle state changes", () => {
+    //testing for state
+    const wrapper = shallow(<Link title="mockTitle" url="mockUrl" />);
+    expect(wrapper.state().clicked).toEqual(false);
+    wrapper.simulate("click");
+    expect(wrapper.state().clicked).toEqual(true);
+  });
+
+  it("should render props correctly", () => {
+    const wrapper = shallow(
+      <Link url="https://www.zeolearn.com" title="zeolearn" />
+    );
+    expect(wrapper.props().href).toEqual("https://www.zeolearn.com");
+    expect(wrapper.props().children).toEqual("zeolearn");
+  });
+});
